e93b3fff9211a796db586635d34f3924ad4440ec
Jeff Butler <jeffgbutler@gmail.com>
2022-09-30 10:17:16 -0400

    Initial conversion to .Net 6

--- Program.cs
+++ Program.cs
@@ -1,27 +1,46 @@
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using PaymentCalculator.Services;
+using System.Collections.Generic;
 
-namespace csharp_payment_calculator
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "csharp_payment_calculator", Version = "v1" });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureHostConfiguration(b =>
-                {
-                    var reloadFlagConfig = new Dictionary<string, string>() {{ "hostbuilder:reloadConfigOnChange", "false" }};
-                    b.AddInMemoryCollection(reloadFlagConfig);
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Services.AddCors();
+builder.Services.AddOptions();
+builder.Services.AddSingleton<PaymentService>();
+builder.Services.AddSingleton<CrashService>();
+builder.Services.AddSingleton<IHitCounterService, MemoryHitCounterService>();
+
+var reloadFlagConfig = new Dictionary<string, string>() {{ "hostbuilder:reloadConfigOnChange", "false" }};
+builder.Configuration.AddInMemoryCollection(reloadFlagConfig);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "csharp_payment_calculator v1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseCors(c => c.AllowAnyOrigin());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
