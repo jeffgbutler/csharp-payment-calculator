e41a612f97460278b1b25fa14203321631e277c8
Jeff Butler <jeffgbutler@gmail.com>
2022-09-30 09:31:06 -0400

    Turn off configuration reloading

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace csharp_payment_calculator
@@ -12,6 +14,11 @@ namespace csharp_payment_calculator
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(b =>
+                {
+                    var reloadFlagConfig = new Dictionary<string, string>() {{ "hostbuilder:reloadConfigOnChange", "false" }};
+                    b.AddInMemoryCollection(reloadFlagConfig);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
